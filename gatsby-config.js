27a7ecc5f59806c19228d60062be1faecb8c332f
Alex O'Callaghan <aocallaghan@outlook.com>
2019-09-21 14:30:01 +0100

    Update gatsby config title, description, and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alex O'Callaghan`,
+    description: `Personal website of Alex O'Callaghan`,
+    author: `@aocallaghan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
